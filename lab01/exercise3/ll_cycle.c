191a45f06a8a595dabaa4cd0a4d5608995e290b1
Jintao Xu <2199272090@qq.com>
2022-11-16 18:05:36 +0800

    finish [lab01] ex3

--- lab01/exercise3/ll_cycle.c
+++ lab01/exercise3/ll_cycle.c
@@ -3,4 +3,12 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    if(head == NULL || head == NULL) return 0;
+    node *fast = head->next, *slow = head;
+    while(fast != slow){
+        if(fast == NULL || fast->next == NULL) return 0;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return 1;
 }
